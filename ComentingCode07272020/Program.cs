5ae1fc960593ea79ee5c9e2a2b417061c22a21ed
chuck francis <chuck_francis10@yahoo.com>
2020-07-27 11:08:12 -0400

    Add project files.

---
+++ ComentingCode07272020/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComentingCode07272020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comments are written to explain code in an easily  // understanable way
+            //Basially for single lines  // anything after // is considered a comment
+            Console.WriteLine("Normal code is compiled an runs ...");
+            Console.WriteLine("Comments however ...");// do not execute!
+
+            // Comments can be on their own line
+            Console.WriteLine("..."); // or they can share like this
+
+            //However  if you put the // Before a line of code
+            // Console.WriteLine("Then it is consiered a comment");
+            // Console.WriteLine("and won't execute!");
+            /*
+             
+            This is a multi-line comment!
+            Named because, well, it spans SO many lines!
+
+            */
+        }
+    }
+}
